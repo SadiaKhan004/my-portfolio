850246c10025d536345962b7c7a7963968b4a185
SadiaKhan004 <sadiakhanajamali4522@gmail.com>
2025-04-02 18:30:44 +0500

    Fix 404 issue in loading assests

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -34,7 +34,7 @@ export function Projects() {
         className="w-20 h-12 lg:w-24 lg:h-12 rounded-full flex bg-gradient-to-r from-[#05C5D7] to-[#036871] items-center justify-center -mr-2 lg:mr-6 transition-transform transform hover:translate-y-[-5px]"
       >
         <img
-          src="/assets/left-arrow.png"
+          src="/left-arrow.png"
           alt="Scroll Left"
           className="w-7 h-7 lg:w-8 lg:h-8 lg:mr-8"
         />
@@ -48,7 +48,7 @@ export function Projects() {
         <div className="bg-[#000C1C] rounded-3xl text-center text-[#05C5D7] w-60 h-70 transition-transform transform hover:-translate-y-5 shadow-custom3 pt-3 flex-shrink-0 lg:w-60 h-80">
           Blood Donation System
           <img
-            src={"/assets/VitalVeins1.jpg"}
+            src={"/VitalVeins1.jpg"}
             alt="Blood Donation System"
             className="mx-auto mt-8  pb-2 w-42 h-52 rounded-3xl shadow-custom4 lg:w-52 lg:h-60 sm:w-42 h-52"
           />
@@ -57,7 +57,7 @@ export function Projects() {
         <div className="bg-[#000C1C] rounded-3xl text-center text-[#05C5D7] w-60 h-70 transition-transform transform hover:-translate-y-5 shadow-custom3 pt-3 flex-shrink-0 lg:w-60 h-80">
           Personalized Cancer Treatment Plan Generator
           <img
-            src={"/assets/onco.png"}
+            src={"/onco.png"}
             alt="Cancer Treatment"
             className="mx-auto mt-2 w-42 h-52 pb-2 rounded-3xl shadow-custom5 lg:w-52 lg:h-60 sm:w-42 h-52"
           />
@@ -66,7 +66,7 @@ export function Projects() {
         <div className="bg-[#000C1C] rounded-3xl text-center text-[#05C5D7] w-60 h-70 transition-transform transform hover:-translate-y-5 shadow-custom3 pt-3 flex-shrink-0 lg:w-60 h-80">
           Orrery Web Application
           <img
-            src={"/assets/orbitexp.png"}
+            src={"/orbitexp.png"}
             alt="Orrery Web Application"
             className="mx-auto mt-8 w-42 h-52 pb-2 rounded-3xl shadow-custom6 lg:w-52 lg:h-60 sm:w-42 h-52"
           />
@@ -75,7 +75,7 @@ export function Projects() {
         <div className="bg-[#000C1C] rounded-3xl text-center text-[#05C5D7] w-60 h-70 transition-transform transform hover:-translate-y-5 shadow-custom3 pt-3 flex-shrink-0 lg:w-60 h-80">
           E-comerce Application
           <img
-            src={"/assets/sakh.png"}
+            src={"/sakh.png"}
             alt="Sakhi Application"
             className="mx-auto mt-8 w-42 h-52 pb-2 rounded-3xl shadow-custom7 lg:w-52 lg:h-60 sm:w-42 h-52"
           />
@@ -88,7 +88,7 @@ export function Projects() {
         className="w-20 h-12 rounded-full flex items-center justify-center ml:2 lg:ml-4 bg-gradient-to-r from-[#036871] to-[#05C5D7] transition-all transition-transform transform hover:translate-y-[-5px] lg:w-24 lg:h-12"
       >
         <img
-          src="/assets/right-arrow.png"
+          src="/right-arrow.png"
           alt="Scroll Right"
           className="w-7 h-7 ml-0 lg:ml-6 lg:w-8 lg:h-8"
         />
